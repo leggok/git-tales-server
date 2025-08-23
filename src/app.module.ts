628a9ef1c0142a5915b0563f06fc1e51cf092047
leggok <dmytro.palianytsia@gmail.com>
2025-08-23 16:24:47 +0300

    feature: add mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [MongooseModule.forRoot(process.env.MONGO_URI ?? "mongodb://localhost/git-tales")],
+    controllers: [AppController],
+    providers: [AppService]
 })
 export class AppModule {}
