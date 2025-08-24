d37c4418d6cbd7134941bfd4eff92b3745fc2129
leggok <dmytro.palianytsia@gmail.com>
2025-08-24 19:34:37 +0300

    fix: fix model

--- src/commits/commit.schema.ts
+++ src/commits/commit.schema.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
 import { Document } from "mongoose";
 import * as mongoose from "mongoose";
-import * as AutoIncrementFactory from "mongoose-sequence";
+import AutoIncrementFactory from "mongoose-sequence";
 
 @Schema({ collection: "commits" })
 export class Commit extends Document {
